cecb96e84f35b566a61f7cdccbb1792f7db23dfe
chock1991 <chochristinak@gmail.com>
2024-10-01 09:49:09 -0600

    add nav bar and header

---
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import "./Navbar.css"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+export default function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light ">
+            <ul className="navbar-nav d-flex flex-column">
+                <li className="nav-item"><a className="nav-link" href="#">Dashboard</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">My Courses</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Messages</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Reports</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">Settings</a></li>
+            </ul>
+        </nav>
+    );
+};
+
+
+    
