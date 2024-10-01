9bccdb6c20a9f962cf81db5210e4ef7213739667
chock1991 <chochristinak@gmail.com>
2024-10-01 10:55:03 -0600

    merge test

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar() {
         <nav className="navbar navbar-expand-lg navbar-light ">
             <ul className="navbar-nav d-flex flex-column">
                 <li className="nav-item"><a className="nav-link" href="#">Dashboard</a></li>
-                <li className="nav-item"><a className="nav-link" href="#">My Courses</a></li>
+                <li className="nav-item"><a className="nav-link" href="#">New Link</a></li>
                 <li className="nav-item"><a className="nav-link" href="#">Messages</a></li>
                 <li className="nav-item"><a className="nav-link" href="#">Reports</a></li>
                 <li className="nav-item"><a className="nav-link" href="#">Settings</a></li>
