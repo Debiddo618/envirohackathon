d52ef6386c505063c2b72bc7878a14d5f186b292
chock1991 <chochristinak@gmail.com>
2024-10-01 10:50:40 -0600

    test merge

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,13 +4,14 @@ import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
 import Notification from './components/Notification/Notification'
 
-
+//*test
 export default function App() {
   return (
     <>
       <Header />
       <Navbar />
       <Notification/>
+
       </>
 
   );
