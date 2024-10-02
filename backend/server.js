0df2154d0680240543d34b7f7e8a30b7b4ce00f0
chock1991 <chochristinak@gmail.com>
2024-10-02 13:19:57 -0600

    google oauth

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
+const cookieParser = require("cookie-parser"); 
 const app = express();
 const mongoose = require("mongoose");
+const session = require("express-session");
+const passport = require("passport");
+const methodOverride = require("method-override");
+const path = require("path"); 
+
+require("./config/database");
+require("./config/passport");
 
 mongoose.connect(process.env.MONGODB_URI);
 
@@ -11,10 +19,28 @@ mongoose.connection.on("connected", () => {
 });
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser()); 
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use(
+  session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(methodOverride("_method"));
+
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(function (req, res, next) {
+  res.locals.user = req.user;
+  next();
+});
 
 // Routes go here
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
 });
-
