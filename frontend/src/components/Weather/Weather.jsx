49311a743a4de311f351d5c207c0610e2866d51d
Debiddo <dzheng@terpmail.umd.edu>
2024-10-03 12:32:46 -0400

    reverted back

--- frontend/src/components/Weather/Weather.jsx
+++ frontend/src/components/Weather/Weather.jsx
@@ -2,7 +2,59 @@ import { useEffect, useState } from "react";
 import data from "../../data/data.json";
 import styles from "./Weather.module.css";
 
-const Weather = ({high, low, currentWeather}) => {
+const Weather = () => {
+  const [weather, setWeather] = useState([]);
+  const [currentWeather, setCurrentWeather] = useState(null);
+
+  // Highest Daily Temperature
+  const [high, setHigh] = useState(null);
+
+  // Lowest Daily Temperature
+  const [low, setLow] = useState(null);
+
+  const getCurrentWeather = () => {
+    const currentDateTime = new Date().toISOString().split(".")[0] + "Z";
+
+    const currentForecast = weather.reduce((closest, obj) => {
+      const objDateTime = new Date(obj.time);
+      const diff = Math.abs(currentDateTime - objDateTime);
+
+      if (
+        !closest ||
+        diff < Math.abs(currentDateTime - new Date(closest.time))
+      ) {
+        return obj;
+      }
+      return closest;
+    }, null);
+
+    setCurrentWeather(currentForecast);
+  };
+
+  useEffect(() => {
+    const url = `https://api.tomorrow.io/v4/weather/forecast?location=42.3478,-71.0466&apikey=${
+      import.meta.env.VITE_TOMORROW_KEY
+    }`;
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        console.log(data.timelines.hourly[0].values.temperature);
+        setWeather(data.timelines.hourly);
+        const temperatures = data.timelines.hourly.map(
+          (hour) => hour.values.temperature
+        );
+        setHigh(Math.max(...temperatures));
+        setLow(Math.min(...temperatures));
+        getCurrentWeather();
+      });
+  }, []);
+
+  useEffect(() => {
+    if (weather.length > 0) {
+      getCurrentWeather();
+    }
+  }, [weather]);
 
   return (
     <>
