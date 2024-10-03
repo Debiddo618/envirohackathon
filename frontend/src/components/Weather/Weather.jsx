72f628348b487baa521bb52dd84e29a38b39b5a2
Debiddo <dzheng@terpmail.umd.edu>
2024-10-02 20:36:14 -0400

    created the temperature component

--- frontend/src/components/Weather/Weather.jsx
+++ frontend/src/components/Weather/Weather.jsx
@@ -1,9 +1,17 @@
 import { useEffect, useState } from "react";
 import data from "../../data/data.json";
+import styles from "./Weather.module.css";
 
 const Weather = () => {
   const [weather, setWeather] = useState([]);
   const [currentWeather, setCurrentWeather] = useState(null);
+
+  // Highest Daily Temperature
+  const [high, setHigh] = useState(null);
+
+  // Lowest Daily Temperature
+  const [low, setLow] = useState(null);
+
   const getCurrentWeather = () => {
     const currentDateTime = new Date().toISOString().split(".")[0] + "Z";
 
@@ -30,38 +38,54 @@ const Weather = () => {
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
+        console.log(data.timelines.hourly[0].values.temperature);
         setWeather(data.timelines.hourly);
+        const temperatures = data.timelines.hourly.map(
+          (hour) => hour.values.temperature
+        );
+        setHigh(Math.max(...temperatures));
+        setLow(Math.min(...temperatures));
         getCurrentWeather();
       });
   }, []);
-  // Call getCurrentWeather whenever weather data changes
+
   useEffect(() => {
     if (weather.length > 0) {
       getCurrentWeather();
     }
   }, [weather]);
 
-  // Render weather information if available
   return (
     <>
-      <h1>Weather</h1>
-      {currentWeather ? (
-        <>
-          <h1>
-            The current weather is: {data[currentWeather.values.weatherCode]}
-          </h1>
-          <img
-            src={`/assets/images/${currentWeather.values.weatherCode}.png`}
-            alt={data[currentWeather.values.weatherCode]}
-          />
-          <h2>
-            Current Temperature: {currentWeather.values.temperature}°C at{" "}
-            {currentWeather.time}
-          </h2>
-        </>
-      ) : (
-        <h2>Loading current weather...</h2>
-      )}
+      <div className={styles.container}>
+        {currentWeather ? (
+          <>
+            <div className={styles.section1}>
+              <div className={styles.temp}>
+                <p>Todays Forecast</p>
+                <h1>{currentWeather.values.temperature}</h1>
+              </div>
+              <div className={styles.icon}>
+                <img
+                  src={`/assets/images/${currentWeather.values.weatherCode}.png`}
+                  alt={data[currentWeather.values.weatherCode]}
+                />
+              </div>
+            </div>
+            <div className={styles.section2}>
+              <p>High {high}</p>
+              <div className={styles.line}></div>
+              <p>Low {low}</p>
+            </div>
+          </>
+        ) : (
+          <>
+            <h2>Loading current weather...</h2>
+          </>
+        )}
+
+        {/*  */}
+      </div>
     </>
   );
 };
