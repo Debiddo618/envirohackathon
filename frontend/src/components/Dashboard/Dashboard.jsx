4b84880b8a0f3d917586fbdb484b64244bd1c8cd
Debiddo <dzheng@terpmail.umd.edu>
2024-10-04 03:11:16 -0400

    changed the openweathermap api to open-mateo

--- frontend/src/components/Dashboard/Dashboard.jsx
+++ frontend/src/components/Dashboard/Dashboard.jsx
@@ -8,13 +8,23 @@ import { useEffect, useState } from "react";
 const Dashboard = () => {
   const [sunSet, setSunSet] = useState(null);
   const [sunRise, setSunRise] = useState(null);
+
+  // Highest Daily Temperature
+  const [high, setHigh] = useState(null);
+
+  // Lowest Daily Temperature
+  const [low, setLow] = useState(null);
+
   useEffect(() => {
     const url =
       "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&temperature_unit=fahrenheit&precipitation_unit=inch&forecast_days=1";
     fetch(url)
       .then((response) => response.json())
       .then((data) => {
-        console.log(data.daily.sunset[0])
+        console.log(data)
+        console.log(data.daily.temperature_2m_max[0])
+        setHigh(data.daily.temperature_2m_max[0])
+        setLow(data.daily.temperature_2m_min[0])
         setSunSet(data.daily.sunset[0]);
         setSunRise(data.daily.sunrise[0]);
       });
@@ -24,7 +34,7 @@ const Dashboard = () => {
       <div className={`${styles.container}`}>
         <div className={styles.section1}>
           <div className={`${styles.component} ${styles.component1}`}>
-            {/* <Weather /> */}
+            <Weather high={high} low={low} />
           </div>
           <div className={`${styles.component} ${styles.component2}`}>
             <Sunlight sunSet={sunSet} sunRise={sunRise} />
