c265dd04a9fd1fcf337d42761a6675aa1c4e539e
Debiddo <dzheng@terpmail.umd.edu>
2024-10-05 16:45:13 -0400

    Create, Read, and Delete for crops

--- backend/controllers/crops.js
+++ backend/controllers/crops.js
@@ -3,6 +3,18 @@ const verifyToken = require("../middleware/verify-token.js");
 const router = express.Router();
 const Crop = require("../models/crop.js");
 
+
+
+// Create Crops
+router.post('/create', async (req, res) => {
+  try {
+      const createdCrop = await Crop.create(req.body);
+      res.status(201).json(createdCrop);
+  } catch (error) {
+      res.status(500).json({ error: error.message })
+  }
+})
+
 // index to show all crops
 router.get("/", async (req, res) => {
   try {
@@ -12,5 +24,26 @@ router.get("/", async (req, res) => {
     res.status(500).json(error);
   }
 });
+
+
+// Delete route
+router.delete('/:cropId', async (req, res) => {
+  try {
+      const deletedCrop = await Crop.findByIdAndDelete(req.params.cropId)
+      // Add a check for a not found pet
+      if (!deletedCrop) {
+          res.status(404);
+          throw new Error('Crop not found.')
+      }
+      // Return an "OK" status if the pet was deleted successfully
+      res.status(200).json(deletedCrop);
+  } catch (error) {
+      if (res.statusCode === 404) {
+          res.json({ error: error.message })
+      } else {
+          res.status(500).json({ error: error.message })
+      }
+  }
+})
 
 module.exports = router;
--- backend/models/crop.js
+++ backend/models/crop.js
@@ -1,8 +1,8 @@
-
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const cropSchema = new Schema({
+const cropSchema = new Schema(
+  {
     name: {
       type: String,
       required: true,
@@ -10,10 +10,26 @@ const cropSchema = new Schema({
     rain_average: {
       type: Number,
       required: true,
-    }
-  }, 
-  { timestamps: true });
-  
-  const Crop = mongoose.model('Crop', cropSchema);
-  module.exports = Crop;
-  
+    },
+    start_date: {
+      type: Date,
+      required: false,
+    },
+    end_date: {
+      type: Date,
+      required: false,
+    },
+    harvest_date: {
+      type: Date,
+      required: false,
+    },
+    duration: {
+      type: Number, // assuming the duration is in days
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Crop = mongoose.model("Crop", cropSchema);
+module.exports = Crop;
