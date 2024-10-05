9cc277d798d8679c481feb7203bb561babc4d340
Debiddo <dzheng@terpmail.umd.edu>
2024-10-05 18:14:14 -0400

    added a form to create crops

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,8 @@ import { useState, useEffect } from "react";
 import CropChart from "./components/CropChart/CropChart";
 import ForecastRainGraph from "./components/ForecastRainGraph/ForecastRainGraph";
 import RainPage from "./components/RainPage/RainPage";
-import Recommendation from "./components/Recommendation/Recommendation";
+import Reccommendation from "./components/Reccomendation/Recommendation";
+import Crops from "./components/Crops/Crops";
 
 /*--------------------services--------------- */
 import * as authService from "./services/authService";
@@ -60,7 +61,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Landingpage />} />
         <Route path="/cropchart" element={<CropChart />} />
-        <Route path="/rain" element={<RainPage />} />
+        <Route path="rain" element={<RainPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route
           path="/users/signup"
@@ -71,6 +72,71 @@ export default function App() {
           element={<SignInForm setUser={setUser} />}
         />
         <Route path="/rec" element={<Reccommendation />} />
+        <Route path="/crops" element={<Crops handleAddCrop={handleAddCrop} crops={crops}/>} />
+      </Routes>
+    </div>
+  );
+}
+import "./App.css";
+import Header from "./components/Header/Header";
+import Notification from "./components/Notification/Notification";
+import Weather from "./components/Weather/Weather";
+import Sunlight from "./components/Sunlight/Sunlight";
+import WeatherChart from "./components/TempChart/TempChart";
+import Map from "./components/Map/Map";
+import Navbar from "./components/Navbar/Navbar";
+import Dashboard from "./components/Dashboard/Dashboard";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import Landingpage from "./components/Landingpage/Landingpage";
+import SignUpForm from "./components/SignUpForm/SignUpForm";
+import SignInForm from "./components/SignInForm/SignInForm";
+import { useState, useEffect } from "react";
+import CropChart from "./components/CropChart/CropChart";
+import ForecastRainGraph from "./components/ForecastRainGraph/ForecastRainGraph";
+import RainPage from "./components/RainPage/RainPage";
+import Recommendation from "./components/Recommendation/Recommendation";
+
+/*--------------------services--------------- */
+import * as authService from "./services/authService";
+import * as cropService from "./services/cropService";
+
+export default function App() {
+  const [user, setUser] = useState(authService.getUser());
+  const [crops, setCrops] = useState([]);
+
+  const navigate = useNavigate();
+
+  const handleSignout = () => {
+    authService.signout();
+    setUser(null);
+    navigate("/users/signin");
+  };
+
+  useEffect(() => {
+    const fetchAllCrops = async () => {
+      const cropsData = await cropService.index();
+      setCrops(cropsData);
+    };
+    fetchAllCrops();
+  }, []);
+
+  return (
+    <div className="container-fluid">
+      <Navbar user={user} handleSignout={handleSignout} />
+      <Routes>
+        <Route path="/" element={<Landingpage />} />
+        <Route path="/cropchart" element={<CropChart />} />
+        <Route path="/rain" element={<RainPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route
+          path="/users/signup"
+          element={<SignUpForm setUser={setUser} />}
+        />
+        <Route
+          path="/users/signin"
+          element={<SignInForm setUser={setUser} />}
+        />
+        <Route path="/rec" element={<Recommendation />} />
       </Routes>
     </div>
   );
