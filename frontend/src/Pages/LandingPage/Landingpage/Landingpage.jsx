903a23131abad285021474400636c72c2d09a4c7
Debiddo <dzheng@terpmail.umd.edu>
2024-10-06 17:05:56 -0400

    removed header

--- frontend/src/Pages/LandingPage/Landingpage/Landingpage.jsx
+++ frontend/src/Pages/LandingPage/Landingpage/Landingpage.jsx
@@ -1,57 +1,29 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
-import Header from "../../../components/Header/Header";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons"; // Import FontAwesome search icon
+import { Link } from "react-router-dom";
 
 const api_key = "08b8bcb043f87d6a013fd6efe6738296";
 
 export default function LandingPage(props) {
-  const [city, setCity] = useState("");
   const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
-  const handleSearch = async (e) => {
-    e.preventDefault();
-    if (city.trim() === "") {
-      setError("Please enter a city name.");
-      return;
-    }
+  const [city, setCity] = useState("");
 
-    try {
-      setError(null);
-      const response = await fetch(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${api_key}`
-      );
+  const handleChange = (e) => {
+    setCity(e.target.value);
+  };
 
-      if (!response.ok) {
-        throw new Error("City not found. Please enter a valid city name.");
-      }
-
-      const data = await response.json();
-      props.setLong(data.coord.lon);
-      props.setLat(data.coord.lat)
-      
-      const weatherData = {
-        city: city,
-        temperature: data.main.temp,
-        lon: data.coord.lon,
-        lat: data.coord.lat,
-      };
-
-      // Navigate to the dashboard and pass weatherData via state
-      navigate("/rain", { state: weatherData });
-
-    } catch (err) {
-      setError(err.message);
-    }
+  const handleSearch = () => {
+    props.handleSearch(city);
   };
 
   return (
     <>
-      <Header />
       <div className="landing-page">
         <h1 className="forcast-lp">Climate Forcasting For Optimal Harvests</h1>
         <form onSubmit={handleSearch} className="search-form">
@@ -59,12 +31,14 @@ export default function LandingPage(props) {
             <input
               type="text"
               value={city}
-              onChange={(e) => setCity(e.target.value)}
+              onChange={handleChange}
               placeholder="Enter city"
               className="search-input"
             />
             <button type="submit" className="search-icon-btn">
-              <FontAwesomeIcon icon={faSearch} />
+              <Link to="/rain">
+                <FontAwesomeIcon icon={faSearch} />
+              </Link>
             </button>
           </div>
         </form>
