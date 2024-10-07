469792bf46116dc9543d66706528d4e17aec5f0e
Debiddo <dzheng@terpmail.umd.edu>
2024-10-07 01:13:48 -0400

    fixed import error

--- frontend/src/components/LandingPage/Landingpage.jsx
+++ frontend/src/components/LandingPage/Landingpage.jsx
@@ -3,9 +3,7 @@ import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSearch } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
-import styles from "./LandingPage.module.css";
-
-const api_key = "08b8bcb043f87d6a013fd6efe6738296";
+import styles from "./Landingpage.module.css";
 
 export default function LandingPage(props) {
   const [error, setError] = useState(null);
